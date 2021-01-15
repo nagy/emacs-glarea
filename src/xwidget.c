6986579ee38a209fd9ba42337734f95507444601
Jin Xue <csjinxue@outlook.com>
2021-01-15 16:02:29 +0800

    Invoke callbacks with GL buffer

--- src/xwidget.c
+++ src/xwidget.c
@@ -33,6 +33,7 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 #include "termhooks.h"
 #include "window.h"
 #include "process.h"
+#include "buffer.h"
 
 /* Include xwidget bottom end headers.  */
 #ifdef USE_GTK
@@ -2749,6 +2750,9 @@ xwidget_osr_draw_cb (GtkWidget *widget, cairo_t *cr, gpointer data)
       GdkWindow* xwin_widget = gtk_widget_get_window (xv->widget);
       GLXContext glcontext = g_object_get_data (G_OBJECT (xv->widget),
                                                 XG_GL_CONTEXT);
+      struct buffer *buf = XBUFFER (xw->buffer), *old = current_buffer;
+
+      set_buffer_internal (buf);
 
       if (glXMakeCurrent (GDK_WINDOW_XDISPLAY (xwin), GDK_WINDOW_XID (xwin_widget), glcontext))
         {
@@ -2763,6 +2767,8 @@ xwidget_osr_draw_cb (GtkWidget *widget, cairo_t *cr, gpointer data)
 
           glXSwapBuffers (GDK_WINDOW_XDISPLAY (xwin), GDK_WINDOW_XID (xwin_widget));
         }
+
+      set_buffer_internal (old);
 
       return TRUE;
     }
@@ -2780,6 +2786,7 @@ glarea_button_event_cb (GtkWidget      *widget,
                         gpointer        data)
 {
   struct xwidget *xw = g_object_get_data (G_OBJECT (widget), XG_XWIDGET);
+  struct buffer *buf = XBUFFER (xw->buffer), *old = current_buffer;
 
   if (!NILP (xw->mouse_button_cb))
     {
@@ -2797,7 +2804,9 @@ glarea_button_event_cb (GtkWidget      *widget,
           break;
         }
 
+      set_buffer_internal (buf);
       call3 (xw->mouse_button_cb, make_int(event->button), action, xw->private_data);
+      set_buffer_internal (old);
     }
 
   return TRUE;
@@ -2809,10 +2818,13 @@ glarea_motion_notify_event_cb (GtkWidget      *widget,
                                gpointer        data)
 {
   struct xwidget *xw = g_object_get_data (G_OBJECT (widget), XG_XWIDGET);
+  struct buffer *buf = XBUFFER (xw->buffer), *old = current_buffer;
 
   if (!NILP (xw->cursor_pos_cb))
     {
+      set_buffer_internal (buf);
       call3 (xw->cursor_pos_cb, make_fixed_natnum (event->x), make_fixed_natnum (event->y), xw->private_data);
+      set_buffer_internal (old);
     }
 
   return TRUE;
